da804b8dca42769f58953ef799943ae5acc42768
Sandeep Kaur <sandeep.kaur@itechnolabs.tech>
2025-09-04 18:09:27 +0530

    Worked on portfolio design

--- src/Components/Homepage/projects.jsx
+++ src/Components/Homepage/projects.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const images = [
-  { src: "../src/images/priscainternational.png", title: "Prisca International", link: "https://priscainternational.com/" },
-  { src: "../src/images/p11.png", title: "Social Blaze", link: "https://socialblaze.ai/" },
-  { src: "../src/images/p3.png", title: "Vox Catalog", link: "https://voxcatalog.com/login" },
-  { src: "../src/images/p4.png", title: "1370 Roof Top Lounge", link: "https://www.1370rooftoplounge.com/" },
-  { src: "../src/images/p5.png", title: "Erlipro", link: "https://www.erlipro.com/" },
-  { src: "../src/images/p6.png", title: "DM Textle Machinery", link: "https://www.dmtextilemachinery.com/" },
-  { src: "../src/images/p7.png", title: "LogyMy", link: "https://www.logymy.com/" },
-  { src: "../src/images/p8.png", title: "LogyTalks", link: "https://logytalks.com/" },
-  { src: "../src/images/p9.png", title: "Impacting Life", link: "http://impactinglife.itechnolabs.tech/" },
-  { src: "../src/images/p10.png", title: "Capital Vue", link: "https://capital-vue.itechnolabs.co.in/login/" },
-  { src: "../src/images/p2.png", title: "Mind GTC", link: "https://mindgtc.com/" },
-  { src: "../src/images/p12.png", title: "Crowd Buzz", link: "https://crowdbuzz.com/pages/home" },
-  { src: "../src/images/p13.png", title: "Baztel", link: "https://esim-frontend.itechnolabs.tech/" },
+  { src: "../src/assets/images/priscainternational.png", title: "Prisca International", link: "https://priscainternational.com/" },
+  { src: "../src/assets/images/p11.png", title: "Social Blaze", link: "https://socialblaze.ai/" },
+  { src: "../src/assets/images/p3.png", title: "Vox Catalog", link: "https://voxcatalog.com/login" },
+  { src: "../src/assets/images/p4.png", title: "1370 Roof Top Lounge", link: "https://www.1370rooftoplounge.com/" },
+  { src: "../src/assets/images/p5.png", title: "Erlipro", link: "https://www.erlipro.com/" },
+  { src: "../src/assets/images/p6.png", title: "DM Textle Machinery", link: "https://www.dmtextilemachinery.com/" },
+  { src: "../src/assets/images/p7.png", title: "LogyMy", link: "https://www.logymy.com/" },
+  { src: "../src/assets/images/p8.png", title: "LogyTalks", link: "https://logytalks.com/" },
+  { src: "../src/assets/images/p9.png", title: "Impacting Life", link: "http://impactinglife.itechnolabs.tech/" },
+  { src: "../src/assets/images/p10.png", title: "Capital Vue", link: "https://capital-vue.itechnolabs.co.in/login/" },
+  { src: "../src/assets/images/p2.png", title: "Mind GTC", link: "https://mindgtc.com/" },
+  { src: "../src/assets/images/p12.png", title: "Crowd Buzz", link: "https://crowdbuzz.com/pages/home" },
+  { src: "../src/assets/images/p13.png", title: "Baztel", link: "https://esim-frontend.itechnolabs.tech/" },
 ];
 
 const Projects = () => {
